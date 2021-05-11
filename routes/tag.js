c8aa55ac187c6e4d76b1eb3fd7bd70df6705f20f
wankhedeshubham58@gmail.com <wankhedeshubham7798@gmail.com>
2021-05-11 17:30:57 +0530

    pushed dropdown changes

--- routes/tag.js
+++ routes/tag.js
@@ -5,8 +5,8 @@ const TagController = require('../controllers/TagController')
 
 
 router.post('/tag', authPermissions.authPermissions(['1']) ,TagController.createTag)
-router.post('/tags/search',TagController.getAllTag)
-router.get('/tags',TagController.getAllTagDropdown)
+router.post('/tags',TagController.getAllTag)
+router.get('/tagIds',TagController.getAllTagDropdown)
 router.put('/tags:tagId',TagController.updateTag)
 router.get('/tags/:tagId',TagController.getTag)
 router.delete('/tags/:tagId',TagController.deleteTag)
